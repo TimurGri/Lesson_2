90749776e798ea2b5738998012b504e6e3646a99
Timur <tim5521@yandex.ru>
2023-12-25 22:12:43 +0300

    Task 1

--- Program.cs
+++ Program.cs
@@ -1,2 +1,5 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Задача 1: Напишите программу, которая принимает на вход число и проверяет, кратно ли оно одновременно 7 и 23.
+
+int a = Convert.ToInt32(Console.ReadLine());
+if ( a % 7 == 0 && a % 23 == 0) Console.WriteLine($"The number is a multiple of 7 and 23");
+else Console.WriteLine("No");
