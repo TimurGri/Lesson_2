7beb79b920c21129dfd62fd048595615317061e3
Timur <tim5521@yandex.ru>
2023-12-25 22:49:43 +0300

    Task 3

--- Program.cs
+++ Program.cs
@@ -1,13 +1,9 @@
-﻿//Задача 2: Напишите программу, которая принимает на вход координаты точки (X и Y), 
-//причём X ≠ 0 и Y ≠ 0 и выдаёт номер координатной четверти плоскости, в которой находится эта точка.
-int x = Convert.ToInt32(Console.ReadLine());
-int y = Convert.ToInt32(Console.ReadLine());
+﻿//Задача 3: Напишите программу, которая принимает на вход целое число 
+//из отрезка [10, 99] и показывает наибольшую цифру числа.
+int a = Convert.ToInt32(Console.ReadLine());
 
-if (x == 0 || y == 0) Console.WriteLine($"(x or y) = 0");
-else {
-    if (x > 0 && y > 0) Console.WriteLine($"x = {x}, y = {y}, I");
-    else if (x > 0 && y < 0) Console.WriteLine($"x = {x}, y = {y}, II");
-    else if (x < 0 && y < 0) Console.WriteLine($"x = {x}, y = {y}, III");
-    else Console.WriteLine($"x = {x}, y = {y}, IV");
-
+if (a > 10 && a < 99){
+    if (a/10 > a%10) Console.WriteLine($"a = {a}, {a/10}");
+    else Console.WriteLine($"a = {a}, {a%10}");
 }
+else Console.WriteLine("The number is not included in the segment");
